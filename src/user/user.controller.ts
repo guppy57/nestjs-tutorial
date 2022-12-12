1dd08b53911aea46d8a78b4d9f7e81c3a688b131
Armaan Gupta <guptaarmaan@johndeere.com>
2022-12-12 13:40:39 -0600

    Did more stuff

---
+++ src/user/user.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, UseGuards, Patch } from '@nestjs/common';
+import { JwtGuard } from '../auth/guard';
+import { GetUser } from '../auth/decorator';
+import { User } from '@prisma/client';
+
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+    constructor() {}
+
+    @Get('me')
+    getMe(@GetUser() user: User) {
+        return user;
+    }
+
+    @Patch()
+    editUser() {}
+}
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,76 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { PrismaService } from '../src/prisma/prisma.service';
+import { AppModule } from '../src/app.module';
+import * as pactum from "pactum";
+import { AuthDto } from 'src/auth/dto';
 
-describe('AppController (e2e)', () => {
+describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+  // imports all modules, as you need all modules for testing
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule]
     }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({ whitelist: true }),
+    );
+
+    await app.listen(4001);
+
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
+    pactum.request.setBaseUrl('http://127.0.0.1:4001');
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    app.close();
   });
-});
+
+  describe('Auth', () => {
+    const dto: AuthDto = { email: "armaan@guppy3.im", password: "123456" };
+    
+    describe('Signup', () => {
+      it("should throw if email empty", () => {
+        return pactum.spec().post('/auth/signup').withBody({ password: dto.password }).expectStatus(400);
+      });
+
+      it("should throw error if email is already in use", () => {
+        const _dto: AuthDto = {
+          email: "armaan@guppyXXX.im",
+          password: "123456"
+        };
+
+        pactum.spec().post('/auth/signup').withBody(_dto).expectStatus(201);
+        return pactum.spec().post('/auth/signup').withBody(_dto).expectStatus(403).inspect();
+      }); 
+
+      it("should signup", () => {
+        return pactum.spec().post('/auth/signup').withBody(dto).expectStatus(201);
+      });
+    });
+
+    describe('Signin', () => {
+      it("should signin", () => {
+        return pactum.spec().post('/auth/signin').withBody(dto).expectStatus(201);
+      });
+    });
+  });
+
+  describe('User', () => {
+    describe('Get me', () => {});
+    describe('Edit user', () => {});
+  });
+
+  describe('Bookmark', () => {
+    describe('Create bookmark', () => {});
+    describe('Edit bookmark', () => {});
+    describe('Delete bookmark', () => {});
+    describe('Get bookmarks', () => {});
+    describe('Get bookmark by id', () => {});
+  });
+})
